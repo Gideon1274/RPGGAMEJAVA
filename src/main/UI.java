8bf07383a4a68822b1780532486b10aa421ed6e8
Gideon1274 <gideongrid127@gmail.com>
2024-01-09 04:27:43 +0800

    modified:   .vscode/launch.json 	modified:   src/entity/Entity.java 	modified:   src/entity/Player.java 	modified:   src/main/AssetSetter.java 	new file:   src/main/EventHandler.java 	new file:   src/main/EventRect.java 	modified:   src/main/GamePanel.java 	modified:   src/main/KeyHandler.java 	new file:   src/main/RectangleGUI.java 	modified:   src/main/UI.java 	new file:   src/object/OBJ_Door.java 	new file:   src/object/OBJ_Heart.java 	deleted:    src/object/SuperObject.java 	modified:   src/pics/maps/worldV2.txt 	modified:   src/tile/TileManager.java

--- src/main/UI.java
+++ src/main/UI.java
@@ -6,16 +6,23 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 import java.text.DecimalFormat;
+
+import entity.Entity;
+import object.OBJ_Heart;
+
+
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.FontFormatException;
+
 
 public class UI {
 
     GamePanel gp;
     Graphics2D g2;
     Font purisaBold, maruMonica;
+    BufferedImage heart_full, heart_half, heart_blank;
     public boolean messageOn = false;
     public String message = "";
     int messageCounter = 0;
@@ -24,7 +31,7 @@ public class UI {
     double playTime;
     public String currentDialogue = "";
     public int commandNum = 0;
-
+    public int titleScreenState = 0; //0 = the first screen, 1 for second screeen
 
     public UI(GamePanel gp){
         this.gp = gp;
@@ -40,6 +47,12 @@ public class UI {
             e.printStackTrace();
         }
        
+        //create hud object
+        Entity heart = new OBJ_Heart(gp);
+        heart_full = heart.image;
+        heart_half = heart.image2;
+        heart_blank = heart.image3;
+
         // arial_40 = new Font("Georgia", Font.PLAIN, 40);
         // arial_80B = new Font("Arial", Font.BOLD, 80);
     }
@@ -57,64 +70,142 @@ public class UI {
         if(gp.gameState == gp.titleState){
             drawTitleScreen();
         }
+        //play state
         if(gp.gameState == gp.playState){
-
+            drawPlayerLife();
         }
+        //pause state
         if(gp.gameState == gp.pauseState){
+            drawPlayerLife();
             drawPauseScreen();
         }
+        //dialogue state
         if(gp.gameState == gp.dialogueState){
+            drawPlayerLife();
             drawDialogueScreen();
         }
     }
+    public void drawPlayerLife(){
+        int x = gp.tileSize/2;
+        int y = gp.tileSize/2;
+        int i = 0;
+        //draw blank heart
+        while(i  < gp.player.maxLife/2){
+            g2.drawImage(heart_blank, x,y, null);
+            i++;
+            x+=gp.tileSize;
+        }
+
+        // reset
+        x = gp.tileSize/2;
+        y = gp.tileSize/2;
+        i = 0;
+
+        //draw current life
+        while(i< gp.player.life){
+            g2.drawImage(heart_half,x,y, null);
+            i++;
+            if(i<gp.player.life){
+                g2.drawImage(heart_full, x,y, null);
+            }
+            i++;
+            x+=gp.tileSize;
+        }
+    }   
     public void drawTitleScreen(){
-        g2.setColor(new Color(0,0,0));
-        g2.fillRect(0,0, gp.screenWidth, gp.screenHeight);
-        //title name
-        g2.setFont(g2.getFont().deriveFont(Font.BOLD, 96F));
-        String text = "Blue Boy Adventure";
-        int x = getXforCenteredText(text);
-        int y = gp.tileSize*3;
-        
-        //shadow
-        g2.setColor(Color.gray);
-        g2.drawString(text, x+5, y+5);
 
-        //title screen
-        g2.setColor(Color.white);
-        g2.drawString(text, x, y);
 
-        //blue boy image
-        x = gp.screenWidth/2 - (gp.tileSize*2)/2;
-        y += gp.tileSize*2;
-        g2.drawImage(gp.player.down1, x, y, gp.tileSize*2, gp.tileSize*2, null);
+        if(titleScreenState==0){
+            
+            g2.setColor(new Color(0,0,0));
+            g2.fillRect(0,0, gp.screenWidth, gp.screenHeight);
+            //title name
+            g2.setFont(g2.getFont().deriveFont(Font.BOLD, 96F));
+            String text = "Blue Boy Adventure";
+            int x = getXforCenteredText(text);
+            int y = gp.tileSize*3;
+            
+            //shadow
+            g2.setColor(Color.gray);
+            g2.drawString(text, x+5, y+5);
 
-        //menu
-        g2.setFont(g2.getFont().deriveFont(Font.BOLD,48F));
+            //title screen
+            g2.setColor(Color.white);
+            g2.drawString(text, x, y);
 
-        text = "NEW GAME";
-        x = getXforCenteredText(text);
-        y+=gp.tileSize*3.5;
-        g2.drawString(text, x, y);
-        if(commandNum ==0){
-            g2.drawString(">", x - gp.tileSize,y );
+            //blue boy image
+            x = gp.screenWidth/2 - (gp.tileSize*2)/2;
+            y += gp.tileSize*2;
+            g2.drawImage(gp.player.down1, x, y, gp.tileSize*2, gp.tileSize*2, null);
+
+            //menu
+            g2.setFont(g2.getFont().deriveFont(Font.BOLD,48F));
+
+            text = "NEW GAME";
+            x = getXforCenteredText(text);
+            y+=gp.tileSize*3.5;
+            g2.drawString(text, x, y);
+            if(commandNum ==0){
+                g2.drawString(">", x - gp.tileSize,y );
+            }
+
+            text = "LOAD GAME";
+            x = getXforCenteredText(text);
+            y+=gp.tileSize;
+            g2.drawString(text, x, y);
+            if(commandNum ==1){
+                g2.drawString(">", x - gp.tileSize,y );
+            }
+            
+            text = "QUIT";
+            x = getXforCenteredText(text);
+            y+=gp.tileSize;
+            g2.drawString(text, x, y);
+            if(commandNum ==2){
+                g2.drawString(">", x - gp.tileSize,y );
+            }
+        }else if (titleScreenState == 1){
+            g2.setColor(Color.white);
+            g2.setFont(g2.getFont().deriveFont(42F));
+
+            String text = "Select your class!";
+            int x  = getXforCenteredText(text);
+            int y = gp.tileSize * 3;
+            g2.drawString(text, x, y);
+
+            text = "Fighter";
+            x = getXforCenteredText(text);
+            y+= gp.tileSize*3;
+            g2.drawString(text, x, y);
+            if(commandNum == 0){
+                g2.drawString(">", x-gp.tileSize, y);
+            }
+            
+            text = "Thief";
+            x = getXforCenteredText(text);
+            y+= gp.tileSize;
+            g2.drawString(text, x, y);
+            if(commandNum == 1){
+                g2.drawString(">", x-gp.tileSize, y);
+            }
+
+            text = "Sorcerer";
+            x = getXforCenteredText(text);
+            y+= gp.tileSize;
+            g2.drawString(text, x, y);
+            if(commandNum == 2){
+                g2.drawString(">", x-gp.tileSize, y);
+            }
+
+            text = "Back";
+            x = getXforCenteredText(text);
+            y+= gp.tileSize*2;
+            g2.drawString(text, x, y);
+            if(commandNum == 3){
+                g2.drawString(">", x-gp.tileSize, y);
+            }
         }
 
-        text = "LOAD GAME";
-        x = getXforCenteredText(text);
-        y+=gp.tileSize;
-        g2.drawString(text, x, y);
-        if(commandNum ==1){
-            g2.drawString(">", x - gp.tileSize,y );
-        }
-        
-        text = "QUIT";
-        x = getXforCenteredText(text);
-        y+=gp.tileSize;
-        g2.drawString(text, x, y);
-        if(commandNum ==2){
-            g2.drawString(">", x - gp.tileSize,y );
-        }
         
     }
     public void drawPauseScreen(){
---
+++ src/object/OBJ_Door.java
@@ -0,0 +1,20 @@
+package object;
+
+import entity.Entity;
+import main.GamePanel;
+
+public class OBJ_Door extends Entity{
+    public OBJ_Door(GamePanel gp){
+        super(gp);
+        name = "Door";
+        down1 = setup("/pics/objects/door");
+        collision = true;
+        solidArea.x=0;
+        solidArea.y= 0;
+        solidArea.width = 36;
+        solidArea.height = 26;
+        solidAreaDefaultX = solidArea.x;
+        solidAreaDefaultY = solidArea.y;
+
+    }
+}
