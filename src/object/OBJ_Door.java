1b6b2680da8e590018a2b49353e64cd9350c8725
Gideon1274 <gideongrid127@gmail.com>
2024-01-10 13:59:11 +0800

    modified:   src/entity/Entity.java 	modified:   src/entity/NPC_OldMan.java 	modified:   src/entity/Player.java 	modified:   src/main/KeyHandler.java 	modified:   src/monster/MON_GreenSlime.java 	modified:   src/object/OBJ_Door.java 	modified:   src/object/OBJ_Heart.java

--- src/object/OBJ_Door.java
+++ src/object/OBJ_Door.java
@@ -7,7 +7,7 @@ public class OBJ_Door extends Entity{
     public OBJ_Door(GamePanel gp){
         super(gp);
         name = "Door";
-        down1 = setup("/pics/objects/door");
+        down1 = setup("/pics/objects/door", gp.tileSize, gp.tileSize);
         collision = true;
         solidArea.x=0;
         solidArea.y= 0;
